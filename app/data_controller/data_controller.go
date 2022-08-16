dc2a53edd8376fe981c98a7734704416cee076c2
yuyuLT <wawawawadaiko@icloud.com>
2022-08-16 22:38:40 +0900

    done go fmt

--- app/data_controller/data_controller.go
+++ app/data_controller/data_controller.go
@@ -1,89 +1,89 @@
 package data_controller
 
 import (
+	"database/sql"
+	"example.com/module/db_operation"
 	"fmt"
 	"log"
 	"os"
 	"time"
-	"database/sql"
-	"example.com/module/db_operation"
 )
 
-type User struct{
-	Id int
+type User struct {
+	Id       int
 	Password string
-	Name string
+	Name     string
 }
 
-type Task struct{
-	Id int
-	UserId int
-	Date time.Time
+type Task struct {
+	Id      int
+	UserId  int
+	Date    time.Time
 	Content string
 }
 
-func CreateTables(){
+func CreateTables() {
 	db_operation.CreateTables()
 }
 
-func InsertUser(id int, password string, name string){
+func InsertUser(id int, password string, name string) {
 	db_operation.InsertUser(id, password, name)
 	fmt.Println("新規登録に成功しました")
 }
 
-func Login(id int, password string){
-	_, pass, _ := SelectUser(id);
-	if pass == password{
+func Login(id int, password string) {
+	_, pass, _ := SelectUser(id)
+	if pass == password {
 		fmt.Println("ログインに成功しました")
-	}else{
+	} else {
 		fmt.Println("ログインに失敗しました")
 		os.Exit(0)
 	}
-};
+}
 
-func SelectUser(id int)(int,string,string){
+func SelectUser(id int) (int, string, string) {
 	row := db_operation.SelectUser(id)
 	var user User
-    err := row.Scan(&user.Id, &user.Password, &user.Name)
-    if err != nil {
-        if err == sql.ErrNoRows {
-            log.Println("No row")
-        } else {
-            log.Println(err)
-        }
-    }
-    return user.Id, user.Password, user.Name
+	err := row.Scan(&user.Id, &user.Password, &user.Name)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			log.Println("No row")
+		} else {
+			log.Println(err)
+		}
+	}
+	return user.Id, user.Password, user.Name
 }
 
-func ShowPost(id int){
-    rows:= db_operation.GetTasks(id)
-    var tasks []Task
+func ShowPost(id int) {
+	rows := db_operation.GetTasks(id)
+	var tasks []Task
 
-    for rows.Next() {
-        var task Task
-        err := rows.Scan(&task.Id,&task.UserId,&task.Date,&task.Content)
-        if err != nil {
-            log.Println(err)
-        }
-        tasks = append(tasks, task)
-    }
-    err := rows.Err()
-    if err != nil {
-        log.Fatalln(err)
-    }
+	for rows.Next() {
+		var task Task
+		err := rows.Scan(&task.Id, &task.UserId, &task.Date, &task.Content)
+		if err != nil {
+			log.Println(err)
+		}
+		tasks = append(tasks, task)
+	}
+	err := rows.Err()
+	if err != nil {
+		log.Fatalln(err)
+	}
 
-    for i, task := range tasks {
+	for i, task := range tasks {
 		number := i + 1
-        fmt.Println(number,task.Content)
-    }
+		fmt.Println(number, task.Content)
+	}
 }
 
-func InsertPost(id int, content string){
+func InsertPost(id int, content string) {
 	db_operation.InsertPost(id, content)
 	fmt.Println("タスクを登録しました")
 }
 
-func DeletePost(id int, task_num int){
-	db_operation.DeletePost(id, task_num - 1)
+func DeletePost(id int, task_num int) {
+	db_operation.DeletePost(id, task_num-1)
 	fmt.Println("タスクを削除しました")
 }
